daea3efda691bc469c995f569a456ee58cff7f8c
Viet Pham <phamhongviet52@gmail.com>
2016-01-25 15:59:05 +0700

    lint

--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 	"strings"
 )
 
-var PORT *string
-var NAME *string
+var port *string
+var name *string
 
-func root_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, *NAME)
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, *name)
 }
 
-func health_handler(w http.ResponseWriter, r *http.Request) {
+func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "OK")
 }
 
-func environ_handler(w http.ResponseWriter, r *http.Request) {
+func environHandler(w http.ResponseWriter, r *http.Request) {
 	var env string
 	for _, e := range os.Environ() {
 		if strings.HasPrefix(e, "DUMMY_") {
@@ -30,14 +30,14 @@ func environ_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func init() {
-	PORT = flag.String("p", "8888", "listen port")
-	NAME = flag.String("n", "Dummy Web Service", "service name")
+	port = flag.String("p", "8888", "listen port")
+	name = flag.String("n", "Dummy Web Service", "service name")
 }
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", root_handler)
-	http.HandleFunc("/environ", environ_handler)
-	http.HandleFunc("/health", health_handler)
-	http.ListenAndServe(":"+*PORT, nil)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/environ", environHandler)
+	http.HandleFunc("/health", healthHandler)
+	http.ListenAndServe(":"+*port, nil)
 }
