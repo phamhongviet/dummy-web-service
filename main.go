a8da498ea3d4c59cc891af11aa84fd796c92450d
Viet Pham <phamhongviet52@gmail.com>
2016-01-25 15:53:32 +0700

    add environ handler

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 )
 
 var PORT *string
@@ -17,6 +19,16 @@ func health_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "OK")
 }
 
+func environ_handler(w http.ResponseWriter, r *http.Request) {
+	var env string
+	for _, e := range os.Environ() {
+		if strings.HasPrefix(e, "DUMMY_") {
+			env += e
+		}
+	}
+	fmt.Fprintf(w, env)
+}
+
 func init() {
 	PORT = flag.String("p", "8888", "listen port")
 	NAME = flag.String("n", "Dummy Web Service", "service name")
@@ -25,6 +37,7 @@ func init() {
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", root_handler)
+	http.HandleFunc("/environ", environ_handler)
 	http.HandleFunc("/health", health_handler)
 	http.ListenAndServe(":"+*PORT, nil)
 }
